a4467e65c32dea080f55850a0a1b648c39efcd35
imgsrc <forifelse@yandex.ru>
2018-08-22 18:50:19 +0300

    added db.json, material-ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'material-components-web/dist/material-components-web.css';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
