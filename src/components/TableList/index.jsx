d1f5a7fb19ec5d02ef8f5a5d41604d530a599e7b
imgsrc <forifelse@yandex.ru>
2018-08-26 14:28:57 +0300

    added table

--- src/components/TableList/index.jsx
+++ src/components/TableList/index.jsx
@@ -17,10 +17,16 @@ export default function TableList() {
 
         <tbody className="table-body">
         <tr>
-          <td className="table-body__item"></td>
-          <td className="table-body__item"></td>
-          <td className="table-body__item"></td>
-          <td className="table-body__item"></td>
+          <td className="table-body__item">100$</td>
+          <td className="table-body__item">req-12311</td>
+          <td className="table-body__item">2018-08-20 - 2018-09-01</td>
+          <td className="table-body__item">2</td>
+        </tr>
+        <tr>
+          <td className="table-body__item">124$</td>
+          <td className="table-body__item">req-12312</td>
+          <td className="table-body__item">2018-08-20 - 2018-09-01</td>
+          <td className="table-body__item">3</td>
         </tr>
         </tbody>
       </Table>
