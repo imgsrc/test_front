12ee82db3f74e691d54d91f164add9cc65e26086
imgsrc <forifelse@yandex.ru>
2018-08-27 18:02:03 +0300

    Customize TextField

--- src/components/CreateRequestForm/index.jsx
+++ src/components/CreateRequestForm/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 import InputField from '../../components/InputField'
+import Button from '../../components/Button'
 
 import './index.css'
 
 export default function CreateRequestForm({ onSubmit }) {
   return (
       <form className="mdc-form-field" onSubmit={ onSubmit }>
-        <InputField label="Price" type="text"/>
+        <InputField label="Price" start text="$" type="text"/>
         <InputField label="Passengers" type="text"/>
-        <InputField label="From/Until" type="text"/>
-        <button type="submit" className="mdc-button">Create</button>
+        <InputField label="From/Until" icon="date_range" end type="text"/>
+        <Button type="submit" className="mdc-button">Create</Button>
       </form>
   );
 }
--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,26 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '../../components/Button';
 import TextField from '@material-ui/core/TextField';
 
-const currencies = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
-
-export default class InputField extends React.Component {
+export default class InputField extends Component {
   state = {
     name: 'Cat in the Hat',
     age: '',
@@ -34,19 +19,67 @@ export default class InputField extends React.Component {
     });
   };
 
+  handleChange = event => {
+    let title = event.target.value;
+
+    this.setState({ title });
+  };
+
+  createInputProps = () => {
+    let { icon, text, start, end } = this.props;
+    if ( icon && start ) {
+      return (
+          {
+            startAdornment: (
+                <InputAdornment position='start'>
+                  <Button className={ `${icon} icon` } icon={ icon }/>
+                </InputAdornment>
+            )
+          }
+      );
+    } else if ( icon && end ) {
+      return (
+          {
+            endAdornment: (
+                <InputAdornment position='end'>
+                  <Button className={ `${icon} icon` } icon={ icon }/>
+                </InputAdornment>
+            )
+          }
+      );
+    } else if ( text && start ) {
+      return (
+          {
+            startAdornment: (
+                <InputAdornment position='start'>
+                  <i className="material-icons">{ text }</i>
+                </InputAdornment>
+            )
+          }
+      );
+    } else if ( text && end ) {
+      return (
+          {
+            endAdornment: (
+                <InputAdornment position='end'>
+                  <i className="material-icons">{ text }</i>
+                </InputAdornment>
+            )
+          }
+      );
+    }
+  };
+
   render() {
-    const { label } = this.props;
+    const { label, icon, start, end } = this.props;
 
     return (
-        <TextField
-            id="full-width"
-            label={ label }
-            InputLabelProps={ {
-              shrink: true,
-            } }
-            fullWidth
-            margin="normal"
-        />
+        <FormControl fullWidth>
+          <label>{ label }</label>
+          <TextField
+              InputProps={ this.createInputProps({ icon }) }
+          />
+        </FormControl>
     );
   }
 }
